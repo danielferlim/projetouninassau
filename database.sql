2d2101ae1fa4b8e406a9c9eccdb914c5378d2d70
Daniel Ferlim <danielferlim@gmail.com>
2022-05-30 18:50:00 -0300

    atualizando db

--- database.sql
+++ database.sql
@@ -1,23 +1,81 @@
--- Script de criação básica do banco!
-USE `projetonassau_db`;
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: projetonassau_db
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.7.3-MariaDB-1:10.7.3+maria~focal
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chamados`
+--
+
+DROP TABLE IF EXISTS `chamados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `chamados` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pergunta` text DEFAULT NULL,
   `nome` varchar(100) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `token` varchar(255) DEFAULT NULL,
+  `resposta` int(11) DEFAULT NULL,
+  `resposta_admin` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `chamados`
+--
+
+LOCK TABLES `chamados` WRITE;
+/*!40000 ALTER TABLE `chamados` DISABLE KEYS */;
+INSERT INTO `chamados` VALUES (18,'Vamos criar um chamado!','Daniel','danielferlim@outlook.com','QksXHx',1,'Enviando uma resposta para o cliente!');
+/*!40000 ALTER TABLE `chamados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `interacao_chamado`
+--
+
+DROP TABLE IF EXISTS `interacao_chamado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `interacao_chamado` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_chamado` varchar(255) DEFAULT NULL,
   `mensagem` text DEFAULT NULL,
   `admin` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `interacao_chamado`
+--
+
+LOCK TABLES `interacao_chamado` WRITE;
+/*!40000 ALTER TABLE `interacao_chamado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `interacao_chamado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -25,4 +83,30 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','$2y$10$1yLJCt2DPARVda.i5H4k3.Gmed.GTPobegC.0zVdFmn7qwLXcpz8G',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'projetonassau_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-30 18:48:09
